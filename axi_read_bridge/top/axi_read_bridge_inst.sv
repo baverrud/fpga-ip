cb5ef07af84d6f4b6db761c25d6744c571cbf886
unknown <F:\SG1\zfs2\sorted_\_usr\TheBat>
2026-09-02 22:47:13 +0200

    fix: make integration templates instantiate cores

--- axi_read_bridge/top/axi_read_bridge_inst.sv
+++ axi_read_bridge/top/axi_read_bridge_inst.sv
@@ -1,12 +1,8 @@
 //-----------------------------------------------------------------------
 //Filename         : axi_read_bridge_inst.sv
-//Description      : Instantiation template for axi_read_bridge_top.
-//                 : The arrayed client req/rsp ports are packed with the
-//                 : client index in the OUTER dimension; this wrapper
-//                 : reverses the outer dimension so SV index j maps to
-//                 : VHDL index j (same idiom as axi_ar_mux_top.sv).
-//                 : All generics are passed through to the VHDL top via
-//                 : mixed-language binding.
+//Description      : Instantiation template for axi_read_bridge.
+//                 : Declares internal signals and instantiates the VHDL
+//                 : core directly through mixed-language binding.
 //Author           : Rune Baeverrud
 //Current Revision : 1.00
 //Licensing        : Zero-Clause BSD (0BSD)
@@ -39,7 +35,6 @@ module axi_read_bridge_inst #(
   logic [GC_ID_WIDTH-1:0] ar_id;
   logic [GC_ADDR_WIDTH-1:0] ar_addr;
   logic [GC_NATIVE_ARLEN_WIDTH-1:0] ar_len;
-  logic [2:0] ar_size;
   logic ar_valid;
   logic ar_ready;
   logic [GC_ID_WIDTH-1:0] r_id;
@@ -49,81 +44,40 @@ module axi_read_bridge_inst #(
   logic r_valid;
   logic r_ready;
 
-
-    localparam int unsigned C_CLIENT_ARLEN_WIDTH = GC_NATIVE_ARLEN_WIDTH -
-        $clog2(GC_CLIENT_DATA_BYTES / GC_NATIVE_DATA_BYTES);
-
-  // Reversed internal vectors feeding the VHDL top (SV index j -> VHDL index j).
-    logic [GC_NUM_CLIENTS-1:0][GC_ADDR_WIDTH-1:0]          req_addr_v;
-    logic [GC_NUM_CLIENTS-1:0][C_CLIENT_ARLEN_WIDTH-1:0]   req_len_v;
-    logic [GC_NUM_CLIENTS-1:0]                             req_valid_v;
-    logic [GC_NUM_CLIENTS-1:0]                             req_ready_v;
-    logic [GC_NUM_CLIENTS-1:0][8*GC_CLIENT_DATA_BYTES-1:0] rsp_data_v;
-    logic [GC_NUM_CLIENTS-1:0][1:0]                        rsp_resp_v;
-    logic [GC_NUM_CLIENTS-1:0]                             rsp_last_v;
-    logic [GC_NUM_CLIENTS-1:0]                             rsp_valid_v;
-    logic [GC_NUM_CLIENTS-1:0]                             rsp_ready_v;
-
-    genvar c;
-    generate
-        for (c = 0; c < GC_NUM_CLIENTS; c++) begin : g_reverse_clients
-            assign req_addr_v[GC_NUM_CLIENTS-1-c]  = req_addr[c];
-            assign req_len_v[GC_NUM_CLIENTS-1-c]   = req_len[c];
-            assign req_valid_v[GC_NUM_CLIENTS-1-c] = req_valid[c];
-            assign req_ready[c] = req_ready_v[GC_NUM_CLIENTS-1-c];
-            assign rsp_data[c]  = rsp_data_v[GC_NUM_CLIENTS-1-c];
-            assign rsp_resp[c]  = rsp_resp_v[GC_NUM_CLIENTS-1-c];
-            assign rsp_last[c]  = rsp_last_v[GC_NUM_CLIENTS-1-c];
-            assign rsp_valid[c] = rsp_valid_v[GC_NUM_CLIENTS-1-c];
-            assign rsp_ready_v[GC_NUM_CLIENTS-1-c] = rsp_ready[c];
-        end
-    endgenerate
-
-  // Instantiates the VHDL axi_read_bridge_top (mixed-language binding).
-    axi_read_bridge_top #(
-        .GC_NUM_CLIENTS        (GC_NUM_CLIENTS),
-        .GC_ADDR_WIDTH         (GC_ADDR_WIDTH),
-        .GC_ID_WIDTH           (GC_ID_WIDTH),
-        .GC_CLIENT_DATA_BYTES  (GC_CLIENT_DATA_BYTES),
-        .GC_NATIVE_DATA_BYTES  (GC_NATIVE_DATA_BYTES),
-        .GC_NATIVE_ARLEN_WIDTH (GC_NATIVE_ARLEN_WIDTH),
-        .GC_CLIENT_FIFO_DEPTH  (GC_CLIENT_FIFO_DEPTH),
-        .GC_CDC_DEPTH          (GC_CDC_DEPTH),
-        .GC_SYNC_STAGES        (GC_SYNC_STAGES)
-    ) u_core (
-        // Clock / reset
-        .aclk     (aclk),
-        .mem_aclk (mem_aclk),
-        .aresetn  (aresetn),
-
-        // Client request interfaces
-        .req_addr  (req_addr_v),
-        .req_len   (req_len_v),
-        .req_valid (req_valid_v),
-        .req_ready (req_ready_v),
-
-        // Client response interfaces
-        .rsp_data  (rsp_data_v),
-        .rsp_resp  (rsp_resp_v),
-        .rsp_last  (rsp_last_v),
-        .rsp_valid (rsp_valid_v),
-        .rsp_ready (rsp_ready_v),
-
-        // Native AXI read-address channel
-        .ar_id    (ar_id),
-        .ar_addr  (ar_addr),
-        .ar_len   (ar_len),
-        .ar_size  (ar_size),
-        .ar_valid (ar_valid),
-        .ar_ready (ar_ready),
-
-        // Native AXI read-data channel
-        .r_id    (r_id),
-        .r_data  (r_data),
-        .r_resp  (r_resp),
-        .r_last  (r_last),
-        .r_valid (r_valid),
-        .r_ready (r_ready)
-    );
+  axi_read_bridge #(
+    .GC_NUM_CLIENTS        (GC_NUM_CLIENTS),
+    .GC_ADDR_WIDTH         (GC_ADDR_WIDTH),
+    .GC_ID_WIDTH           (GC_ID_WIDTH),
+    .GC_CLIENT_DATA_BYTES  (GC_CLIENT_DATA_BYTES),
+    .GC_NATIVE_DATA_BYTES  (GC_NATIVE_DATA_BYTES),
+    .GC_NATIVE_ARLEN_WIDTH (GC_NATIVE_ARLEN_WIDTH),
+    .GC_CLIENT_FIFO_DEPTH  (GC_CLIENT_FIFO_DEPTH),
+    .GC_CDC_DEPTH          (GC_CDC_DEPTH),
+    .GC_SYNC_STAGES        (GC_SYNC_STAGES)
+  ) u_axi_read_bridge (
+    .aclk       (aclk),
+    .mem_aclk   (mem_aclk),
+    .aresetn    (aresetn),
+    .req_addr   (req_addr),
+    .req_len    (req_len),
+    .req_valid  (req_valid),
+    .req_ready  (req_ready),
+    .rsp_data   (rsp_data),
+    .rsp_resp   (rsp_resp),
+    .rsp_last   (rsp_last),
+    .rsp_valid  (rsp_valid),
+    .rsp_ready  (rsp_ready),
+    .ar_id      (ar_id),
+    .ar_addr    (ar_addr),
+    .ar_len     (ar_len),
+    .ar_valid   (ar_valid),
+    .ar_ready   (ar_ready),
+    .r_id       (r_id),
+    .r_data     (r_data),
+    .r_resp     (r_resp),
+    .r_last     (r_last),
+    .r_valid    (r_valid),
+    .r_ready    (r_ready)
+  );
 
 endmodule
